e57a0cac0a7ab40735bef5f20eb84d27dd32b29e
Santhoshkumar_M <santoo1234abcd@gmail.com>
2023-02-22 12:57:26 +0530

    Upadted

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -9,7 +9,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.example.demo.model.LocationStates;
 import com.example.demo.services.CoronaVirusDataServiceRepository;
@@ -18,6 +21,7 @@ import com.example.demo.services.CoronaVirusDataServices;
 @Controller
 public class HomeController {
 
+	@Autowired
 	CoronaVirusDataServices crnService;
 
 	@Autowired
@@ -59,7 +63,61 @@ public class HomeController {
 	
 	@RequestMapping(path = "/collectChartData/country={name}",produces = {"application/json"})
 	@ResponseBody
+	public LocationStates collectChartDataByCountryName(@PathVariable("name") String countryName,Model model)
 	{
-		
+		System.out.println("View Chart Data by Country Name here...");
+		LocationStates locationstates=repository.findBycountry(countryName);
+		return locationstates;
+	}
+	
+	/*
+	 * @RequestMapping(path = "/collectChartData/top={count}",produces =
+	 * {"application/json"})
+	 * 
+	 * @ResponseBody public List<LocationStates>
+	 * collectChartDataByCountryTop(@PathVariable("count") int count,Model model){
+	 * System.out.println("View Chart Data by Count..."); List<LocationStates>
+	 * locationstates=repository.findcountryBylatestTotalDeath(count); return
+	 * locationstates; }
+	 */
+	
+	@RequestMapping(value="/viewChart",method = RequestMethod.GET)
+	public ModelAndView viewChart() {
+		return new ModelAndView("viewChart").addObject("myURL",new String("http://localhost:8082/collectChartData"));
+	}
+
+	@GetMapping("/viewChart/{id}")
+	public String ViewChartById(@PathVariable("id") int id,Model m) {
+		m.addAttribute("id",id);
+		m.addAttribute("myURL", "http://localhost:8082/collectChartData/"+id);
+		return "ViewChart";
+	}
+	
+	@GetMapping("/collectChartData/country={name}")
+	public String ViewChartByCountryName(@RequestParam String name,Model m) {
+		m.addAttribute("myURL", "http://localhost:8082/collectChartDate/country?"+name);
+		return "ViewChart";
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/main/java/com/example/demo/services/CoronaVirusDataServiceRepository.java
+++ src/main/java/com/example/demo/services/CoronaVirusDataServiceRepository.java
@@ -1,9 +1,18 @@
 package com.example.demo.services;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.demo.model.LocationStates;
 
+@Repository
 public interface CoronaVirusDataServiceRepository extends JpaRepository<LocationStates, Integer>{
 
+	LocationStates findBycountry(String countryName);
+
+	/*
+	 * List<LocationStates> findcountryBylatestTotalDeath(int count);
+	 */
 }
